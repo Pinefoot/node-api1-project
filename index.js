415ebd5d334da9e2ad8a27030e5857dea7611257
Pinefoot <jameslcarpino@gmail.com>
2020-05-04 18:31:57 -0700

    patch is working sort of, but not really perfectly

--- index.js
+++ index.js
@@ -7,11 +7,17 @@ server.use(express.json());
 
 let users = [
         {
-            id: shortid.generate(), //import that unique id thing.
+            id: 1, //import that unique id thing.
             name: 'James Carpino', //string reqiured
             bio: 'A human. Who is also an idiot' //string required
 
-    }
+    },
+    {
+        id: 2, //import that unique id thing.
+        name: 'Another Name', //string reqiured
+        bio: 'This exists for deleting' //string required
+
+}
 ]
 
 //gets
@@ -19,21 +25,31 @@ server.get('/', (req, res) =>{
     res.json({api: 'api working and running.'});
 })
 server.get('/api/users', (req, res)=>{
-
-    res.json(users);
+    if(!users){
+        res.status(500).json({errorMessage: "This user information could not be retrieved."})
+    }else{
+    res.json(users)};
 })
 
-server.get('/api/users:id', (req, res)=>{
-    let id = req.params.id;
+server.get('/api/users/:id', (req, res)=>{
+    let id = Number(req.params.id);
+    let userId = users.filter((usersF)=> usersF.id === id)
 
-    if(id !== users.id ){
+    if(!id){
         res.status(404).json({
             message: "The User with the specified ID does not exist."
         })
+        
+    // }else if(!users){
+    //     res.status(500).json({ errorMessage: 'The user information could not be retrieved'});
+    // }else{
+    //     res.status(200).json(userId)
     }else{
-    
-    res.json(id);
+        res.status(200).json(userId)
     }
+    
+    
+    
 })
 
 // get('/:id', (req, res) => {
@@ -53,7 +69,10 @@ server.get('/api/users:id', (req, res)=>{
 
 //post
 server.post('/api/users', (req, res)=>{
-    const newUser = req.body;
+    const newUser = { 
+        id: Number(shortid.generate()),
+        ...req.body
+    };
 
     if( newUser.name === null || newUser.bio === null || newUser.name === '' || newUser.bio === ""){
         res.status(400).json({
@@ -70,31 +89,51 @@ server.post('/api/users', (req, res)=>{
 
 //delete
 server.delete('/api/users/:id', (req, res)=>{
-    let userId = req.params.id;
-
-    if(userId !== users.id){
+    let id = req.params.id;
+    let usersDel = users.filter(usersD => usersD.id != id);
+    
+    if(id === undefined){
         res.status(404).json({message: "The user with the specified ID does not exist."})
     }
     
-    else if(!usersId){
+    else if(!users.id){
         res.status(500).json({errorMessage: "The user could not be removed"})
 
     }
 
     else{
-        users = users.filter(users => users.id != id);
+        
 
-    res.status(200).json(users)
+    res.status(200).json(usersDel)
     }
 
 })
 
 //patch
 server.patch('/api/users/:id', (req, res)=>{
-    let patchID = req.body
+    let patchID = Number(req.params.id);
+    let newUser = req.body;
+
+    newUser.id = shortid.generate();
+    const userId = users.filter((id)=> id.id === id);
+
+    if(newUser.id === users.id){
+        res.status(404).json({message: "The User with the specified ID does not exist."})
+    }
+    else if(newUser.name === null || newUser.name ==='' || newUser.bio ==="" || newUser.bio ===null){
+        res.status(400).json({errorMessage: 'Please provide a name and bio for user.'})
+    }
+    else if(!newUser.id)
+        {
+            res.status(500).json({errorMessage: 'The user information could not be modified'})
+        }
+        else{
+            users.push(newUser)
+            res.status(200).json(newUser);
+        }
 
 
 })
 
 //server listening
-server.listen(8000, ()=> console.log('\n== API IS RUNNING AND WORKED UP!==\n'))
+server.listen(8000, ()=> console.log('\n== API IS RUNNING AND UP!==\n'))
